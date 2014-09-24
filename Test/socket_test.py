3cc5f5c9be6b2559a6a82882facd70b6dd227e34
duxin <duxin@bestpay.com.cn>
2014-09-24 21:28:36 +0800

    socket

--- Test/socket_test.py
+++ Test/socket_test.py
@@ -4,39 +4,59 @@
 
 import sys
 import socket
+import threading
 import thread
+import os
 
-class Request:
+class Request(threading.Thread):
 	"""docstring for ClassName"""
-	def __init__(self, ip, port):
+	def __init__(self, ip, port, count, message):
+		threading.Thread.__init__(self) 
 		self.ip = ip
-		self.prot = port
-		
-	def Call(self, message):
+		self.port = port
+		self.count = count
+		self.message = message
+	
+	def Call(self, message, i):
 		try:
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		except socket.error, msg:
-			print 'Create socket failed:' + msg
+			print 'Create socket failed:%s' %msg
 
 		try:
-			s.connect((ip , port))
+			s.connect((self.ip , self.port))
 		except socket.error, msg:
-			print 'Connect failed:' + msg
+			print 'Connect failed:%s' %msg
 
 		try :
-			s.sendall(message)
+			s.sendall(message.decode('utf-8'))
 		except socket.error, msg:
-			print 'Send failed:' + msg
+			print 'Send failed:%s' %msg
 		#Now receive data
-		ret_message = s.recv(102400)
+		ret_message = ''
+		while 1:
+			try:
+				ret_message = s.recv(2048)
+			except socket.error, msg:
+				print "Received error:%s" %msg
+			if not len(ret_message):
+				break
+		s.close()
+
+		ret_message = '[' + self.getName() + ':' + str(i) + ']' + ret_message + '\n'
 		return ret_message
- 	
- 	def ConcurrentCall(thread_count, round, message):
- 		for i in xrange(thread_count):
- 			thread.start_new_thread(self.Call, (message))
+
+ 	def run(self):
+ 		file = open('./' + self.getName() + '.log', 'w')
+ 		for i in xrange(self.count):
+ 			ret = self.Call(self.message, i)
+ 			file.write(ret)
+ 		file.close()
+ 		
 def test():
-	req = Request('192.168.87.4', '8588')
-	req.ConcurrentCall(10, 10, 'FFFF012345678900000118EBK000101001UU00ABCDEFGHIJKLMNOPQRSTUVWXYZ000000000020010200210004600100220018110000001000101836')
+	for i in xrange(10):
+		Request('192.168.87.4', 8588, 5, 'FFFF012345678900000118EBK000101001UU00ABCDEFGHIJKLMNOPQRSTUVWXYZ000000000020010200210004600100220018110000001000101836').start()
+		
 
 if __name__ == '__main__':
 	test()
