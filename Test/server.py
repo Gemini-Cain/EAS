1f0fc6dfef8fca1498975663c3ac025216ac6165
duxin <dx19890621@gmail.com>
2015-03-24 23:52:43 +0800

    201403242352

--- Test/server.py
+++ Test/server.py
@@ -5,13 +5,18 @@
 import socket
 import os
 import shutil
+import log
+import time
 
 class Server:
 	"""服务端"""
-	def __init__(self, ip, port, message):
+	def __init__(self, ip, port, message, timeout):
 		self.ip = ip
 		self.port = port
 		self.message = message
+		self.timeout = timeout
+		path = './/log//Server//'
+		self.log = log.Log(path, 'Server')
 	
 	def StartServer(self):
 		try:
@@ -19,27 +24,27 @@ class Server:
 			sock.bind((self.ip, self.port))
 			sock.listen(10) 
 		except socket.error, msg:
-			print '[Error] Create socket failed:%s' %msg
+			error_message = 'Create socket failed:' + msg
+			self.log.log_error(error_message)
 		
 		while True:
 			connection,address = sock.accept()
 			try:
 				connection.settimeout(5)
-				buf = connection.recv(10240)
-				print buf
-				if len(buf):
-					connection.send('FFFF012345678900000118EBK000101001UU00ABCDEFGHIJKLMNOPQRSTUVWXYZ000000000020010200210004600100220018110000001000101836')
-			except socket.timeout:
-				print 'time out'
+				buff = connection.recv(10240)
+				self.log.log_info('[<--]' + buff)
+				if len(buff):
+					self.log.log_info('[-->]' + self.message)
+					time.sleep(self.timeout)
+					connection.send(self.message)
+			except socket.timeout, msg:
+				error_message = 'Time out:' + msg
+				self.log.log_error(error_message)
 			connection.close()		
  		
 def test():
-	path = '.\\log\\'
-	if os.path.exists(path):
-		shutil.rmtree(path)  
- 	os.mkdir(path)
  	request_message = 'FFFF012345678900000118EBK000101001UU00ABCDEFGHIJKLMNOPQRSTUVWXYZ000000000020010200210004600100220018110000001000101836';
-	Server('127.0.0.1', 8588, request_message).StartServer()
+	Server('127.0.0.1', 8588, request_message, 20).StartServer()
 
 if __name__ == '__main__':
 	test()
