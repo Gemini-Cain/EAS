7689b65312a9e086dd05168a793f63f7496652a8
duxin <duxin@bestpay.com.cn>
2015-03-27 17:53:57 +0800

    201503271753

--- Test/console.py
+++ Test/console.py
@@ -15,8 +15,8 @@ class Console:
 		pass
 	
 	def StartServer(self):
-		request_message = 'FFFF012345678900000118EBK000101001UU00ABCDEFGHIJKLMNOPQRSTUVWXYZ000000000020010200210004600100220018110000001000101836';
-		server.Server('success_service', '127.0.0.1', 8588, request_message, 1).StartServer()
+		return_message = 'FFFF012345678900000118EBK000101001UU00ABCDEFGHIJKLMNOPQRSTUVWXYZ000000000020010200210004600100220018110000001000101836';
+		server.Server('success_service', '127.0.0.1', 8588, return_message, 1).StartServer()
 		
 	def StopServer(self):
 		pass
@@ -25,11 +25,16 @@ def test():
 	pid = ''
 	command = raw_input("Setting opition(server or client):\n")
 	if command == "server":
-		console = Console()
-		console.StartServer()
+		print 'Start server command: start server -n [server name] -i [ip] -p [port] -m [return_message] -t [timeout]'
+		print 'Stop server command: stop server [server name] | [server id]'
+		print 'Scan server command: list server'
+		opition = raw_input('please input command:').split()
+		for item in opition:
+			print item
+		#console = Console()
+		#console.StartServer()
 
 	command = raw_input("Setting Server:start server or stop server\n")
-	if command == "stop server":
 		
 
 if __name__ == '__main__':
