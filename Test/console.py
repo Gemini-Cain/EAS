aed47c47516e4944f5d1348431d2a9a2b1b239df
duxin <dx19890621@gmail.com>
2015-03-30 02:52:23 +0800

    201503300252

--- Test/console.py
+++ Test/console.py
@@ -14,28 +14,89 @@ class Console:
 	def __init__(self):
 		pass
 	
-	def StartServer(self):
-		return_message = 'FFFF012345678900000118EBK000101001UU00ABCDEFGHIJKLMNOPQRSTUVWXYZ000000000020010200210004600100220018110000001000101836';
-		server.Server('success_service', '127.0.0.1', 8588, return_message, 1).StartServer()
+	def StartServer(self, name, ip, port, return_message, timeout):
+		if name == '':
+			name = 'success_service'
+		if ip == '':
+			ip = '127.0.0.1'
+		if port == '':
+			port = 8588
+		if return_message == '':
+			return_message = 'FFFF012345678900000118EBK000101001UU00ABCDEFGHIJKLMNOPQRSTUVWXYZ000000000020010200210004600100220018110000001000101836'
+		if timeout == '':
+			timeout = 1
+		server.Server(name, ip, port, return_message, timeout).StartServer()
 		
 	def StopServer(self):
 		pass
  		
 def test():
-	pid = ''
-	command = raw_input("Setting opition(server or client):\n")
-	if command == "server":
-		print 'Start server command: start server -n [server name] -i [ip] -p [port] -m [return_message] -t [timeout]'
-		print 'Stop server command: stop server [server name] | [server id]'
-		print 'Scan server command: list server'
-		opition = raw_input('please input command:').split()
-		for item in opition:
-			print item
-		#console = Console()
-		#console.StartServer()
+	server_command = ('start', 'stop', 'list', 'exit')
+	server_start_command_opition= {'-n':'', '-i':'', '-p':'', '-m':'', '-t':''}
+	client_command = ()
+	while True:
+		command = raw_input("Setting opition(server, client or exit): > ")
+		if command == 'server':
+			while True:
+				print '1.Start server command: start -n [server name] -i [ip] -p [port] -m [return_message] -t [timeout]'
+				print '2.Stop server command: stop [server name] | [server id]'
+				print '3.Scan server command: list'
+				print '4.Exit command: exit'
+				opition = raw_input('please input command: > ').split()
+				if len(opition) <= 0:
+					continue
+				elif len(opition) == 1:
+					if opition[0] == server_command[2]:
+						pass
+					elif opition[0] == server_command[3]:
+						break
+					elif opition[0] == server_command[0]:
+						console = Console()
+						console.StartServer('', '', '', '', '')
+					else:
+						print '[!]Error Input'
+						continue
+				elif len(opition) == 2:
+					if opition[0] == server_command[1]:
+						pass
+					else:
+						print '[!]Error Input'
+						continue
+				else :
+					if opition[0] == server_command[0]:
+						if len(opition) % 2 == 0:
+							print '[!]Error Input'
+							continue
+						else:
+							for i in xrange(1,len(opition), 2):
+								if opition[i] in server_start_command_opition:
+									server_start_command_opition[opition[i]] = opition[i + 1]
 
-	command = raw_input("Setting Server:start server or stop server\n")
-		
+						name = server_start_command_opition['-n']
+						ip = server_start_command_opition['-i']
+						port = server_start_command_opition['-p']
+						return_message = server_start_command_opition['-m']
+						timeout = server_start_command_opition['-t']
+						console = Console()
+						console.StartServer(name, ip, port, return_message, timeout)
+											
+					else:
+						print '[!]Error Input'
+						continue
+
+				for item in opition:
+					print item
+				#console = Console()
+				#console.StartServer()	
+		elif command == 'client':
+			pass
+		elif command == 'exit':
+			break
+		else:
+			print '[!]Error Input'
+			continue
+
+
 
 if __name__ == '__main__':
 	test()
